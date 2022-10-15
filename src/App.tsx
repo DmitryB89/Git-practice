03aa66152daab257bc8ab79fb848b8681f29da7d
DmitryB89 <d.baydarov@yandex.ru>
2022-10-15 23:31:51 +0300

    commit 2 - lorem

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ function App() {
   return (
     <div className="App">
     <h1>Git practice</h1>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ex illo laborum nihil odit placeat provident rem vel velit. At, quos!</p>
     </div>
   );
 }
